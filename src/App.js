9b11901bfbb9d7e71f63ac3061c83c1cd16e49b5
serraguti <pacoserranox@gmail.com>
2022-10-24 12:30:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ServicioCustomers from './components/ServicioCustomers';
+import BuscadorCustomer from './components/BuscadorCustomer';
+
+function App() {
+  return (
+    <div className="App">
+      <BuscadorCustomer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Global.js
@@ -0,0 +1,5 @@
+var Global = {
+    urlCustomers: "https://northwind.netcore.io/"
+}
+
+export default Global;
